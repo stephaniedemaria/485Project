05166b9d92e28c87966a038e954ce65f41565269
WestD3 <47015230+WestD3@users.noreply.github.com>
2019-04-23 14:53:51 -0500

    Update twitterapi.py

--- twitterapi.py
+++ twitterapi.py
@@ -1,11 +1,11 @@
 import twitter
 
-ipython
+#ipython
 
 api = twitter.Api(consumer_key='6y72BDZ2J92EUpRDFdpkaNEKX',
 consumer_secret='WIH6SoyIESgycaHm8Zr8jye3jcu54dWW9b9Bqbzs5cNrNhVV2w',
 access_token_key='1118980620598808602-ezBC0CClGot06RNDPpdQ99WyG9RGfr',
-access_token_secrets='INw7C24NJcyT0bFhIPnR3Y5LGvxr9PC1jSNHfPGhqve3T')
+access_token_secret='INw7C24NJcyT0bFhIPnR3Y5LGvxr9PC1jSNHfPGhqve3T')
 
 print(api.VerifyCredentials())
 
