1f38416733e84ecce3d5f5bb227ee65af2d4391d
Emily Alvarado <z1833226@students.niu.edu>
2019-04-23 17:45:27 -0500

    Moron

--- graph.py
+++ graph.py
@@ -2,10 +2,18 @@ import pandas as pd
 tweets = pd.read_csv("archive.csv")
 tweets.head()
 
-
 import numpy as np
 import matplotlib.pyplot as plt
- 
+
+def get_candidate(row):
+    candidates = []
+    text = row["text"].lower()
+    if "moron" in text:
+        candidates.append("moron")
+    return ",".join(candidates)
+tweets["candidate"] = tweet.apply(get_candidate,axis=1)
+
+
 # Make a fake dataset:
 height = [3, 12, 5, 18, 45]
 bars = ('A', 'B', 'C', 'D', 'E')
