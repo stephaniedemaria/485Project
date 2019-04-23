2c2b880c486563a2cd5ddd4bbac463149b0d6457
Emily Alvarado <z1833226@students.niu.edu>
2019-04-23 15:41:58 -0500

    Update

--- twitterapi.py
+++ twitterapi.py
@@ -1,7 +1,6 @@
 import twitter
 
 
-
 api = twitter.Api(consumer_key='6y72BDZ2J92EUpRDFdpkaNEKX',
 consumer_secret='WIH6SoyIESgycaHm8Zr8jye3jcu54dWW9b9Bqbzs5cNrNhVV2w',
 access_token_key='1118980620598808602-ezBC0CClGot06RNDPpdQ99WyG9RGfr',
